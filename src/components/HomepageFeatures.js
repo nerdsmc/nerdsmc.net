f568a817ed1843413cce57328c280e896302f9b5
Sayrix <43046854+Sayrix@users.noreply.github.com>
2021-11-13 20:00:48 +0100

    a

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -39,7 +39,7 @@ function Feature({Image, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <img src={styles.featureImage} alt={title} />;
+        <img src={Image} alt={title} />;
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
