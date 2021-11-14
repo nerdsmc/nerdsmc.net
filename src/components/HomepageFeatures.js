6dc26654e768ed906b4c7ff6469411d6a535b75d
Sayrix <43046854+Sayrix@users.noreply.github.com>
2021-11-14 01:28:53 +0100

    a

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -12,25 +12,13 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Génération 1.18',
+    Image: 'https://cdn.sayrix.fr/sH8/javaw_SDVpFKFkr9.jpg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
       </>
     ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+  }
 ];
 
 function Feature({Image, title, description}) {
