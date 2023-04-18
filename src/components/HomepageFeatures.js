632e8ab5cd9acd924972000075e50159bdcbd350
Sayrix <43046854+Sayrix@users.noreply.github.com>
2023-04-18 16:39:32 +0200

    Fixed broken images

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -5,22 +5,22 @@ import styles from './HomepageFeatures.module.css';
 const FeatureList = [
   {
     title: 'Survival 1.19',
-    Image: 'https://uworldmc.fr/img/survie.jpg',
+    Image: 'https://nerdsmc.net/img/survie.jpg',
     alt: 'Minecraft Server Survival Crack 1.19'
   },
   {
     title: 'MissileWars',
-    Image: 'https://uworldmc.fr/img/missilewars.png',
+    Image: 'https://nerdsmc.net/img/missilewars.png',
     alt: 'Minecraft Server MissileWars Crack'
   },
   {
     title: 'OneBlock 1.19',
-    Image: 'https://uworldmc.fr/img/oneblock.gif',
+    Image: 'https://nerdsmc.net/img/oneblock.gif',
     alt: 'Minecraft Server OneBlock Crack 1.19'
   },
   {
     title: 'Parkour',
-    Image: 'https://uworldmc.fr/img/parkour.gif',
+    Image: 'https://nerdsmc.net/img/parkour.gif',
     alt: 'Minecraft Server Parkour Crack SMP'
   }
 ];
